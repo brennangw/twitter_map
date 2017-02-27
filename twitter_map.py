2bfb606fd24b8c45bccaef28a8788016aa30448d
Brennan Wallace <brennanwallace@dyn-160-39-134-39.dyn.columbia.edu>
2017-02-26 22:01:18 -0500

    elastic search connected and a get request gets all tweets

--- twitter_map.py
+++ twitter_map.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template, request, send_from_directory, send_file, jsonify,app, session
+from flask import send_file, jsonify
 from flask import Flask
+import certifi
+from elasticsearch import Elasticsearch
+import secretsAndSettings as sas
+
+#elasticsearch set up
+elasticsearch = Elasticsearch(sas.elasticSearch['uri'],
+                              port=sas.elasticSearch['port'],
+                              use_ssl=sas.elasticSearch['use_ssl'])
+
+
+
 
 app = Flask(__name__)
+
 
 
 @app.route('/')
 def hello_world():
     #return send_from_directory('html', 'index.html') why doesnt this work??
     return send_file('static/html/index.html')
+
+@app.route('/tweets')
+def getTweets():
+    res = elasticsearch.search(index="tweets", body={"query": {"match_all": {}}})
+    return jsonify(res)
+
+
 
 if __name__ == '__main__':
     app.run()
